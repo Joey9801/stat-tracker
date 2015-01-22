3cdec7c9946e2d24f8d0c266518246bbc520741a
Daniel Richman <main@danielrichman.co.uk>
2015-01-22 21:04:35 +0000

    fix schema

--- schema.sql
+++ schema.sql
@@ -1,5 +1,14 @@
 CREATE TYPE team AS ENUM ('red', 'blue');
 
+CREATE TABLE players (
+    id SERIAL PRIMARY KEY,
+    first_name character varying(50) NOT NULL,
+    last_name character varying(50) NOT NULL,
+    score real DEFAULT 0 NOT NULL,
+    nickname character varying(50) NOT NULL,
+    crsid character varying(10)
+);
+
 CREATE TABLE games (
     id SERIAL PRIMARY KEY,
     red_score integer NOT NULL,
@@ -15,17 +24,8 @@ CREATE TABLE games_players (
     player_id integer NOT NULL REFERENCES players(id),
     team team NOT NULL,
     win boolean NOT NULL,
-    score real NOT NULL,
+    score real,
     PRIMARY KEY (game_id, player_id)
-);
-
-CREATE TABLE players (
-    id SERIAL PRIMARY KEY,
-    first_name character varying(50) NOT NULL,
-    last_name character varying(50) NOT NULL,
-    score real DEFAULT 0 NOT NULL,
-    nickname character varying(50) NOT NULL,
-    crsid character varying(10)
 );
 
 GRANT ALL ON TABLE games TO joe;
