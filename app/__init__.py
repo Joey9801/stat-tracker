d4c8820a890032637c6ac10d94706f86ad9ab11a
Joe Roberts <joe@jwjr.co.uk>
2015-03-16 14:51:00 +0000

    basic game api

--- app/__init__.py
+++ app/__init__.py
@@ -5,3 +5,6 @@ app = Flask(__name__)
 app.config.from_object('config')
 
 db = SQLAlchemy(app)
+
+from .api import game
+app.register_blueprint(game.game, url_prefix='/api/game')
---
+++ app/api/game.py
@@ -0,0 +1,47 @@
+from flask import Blueprint, jsonify, abort
+
+from ..models import Game, GamePlayer
+
+game = Blueprint('game', __name__)
+
+@game.route('/<int:game_id>', methods=['GET'])
+def by_id(game_id):
+    game = Game.query.get(game_id)
+    if game:
+        return jsonify( game.toDict() )
+    else:
+        abort(404)
+
+@game.route('/last_n/<int:n>', methods=['GET'])
+def last_n(n):
+
+    #Prevent pulling whole table
+    if n > 20:
+        abort(403)
+
+    games = (Game.query
+            .order_by( Game.timestamp.desc() )
+            .limit(n)
+            )
+
+    d = {'games': [g.toDict() for g in games] }
+    
+    return jsonify( d )
+
+@game.route('/last_n/<int:n>/<int:player_id>', methods=['GET'])
+def last_n_by_player(n, player_id):
+
+    #Prevent pulling whole table
+    if n > 20:
+        abort(403)
+
+    games = (Game.query
+            .join(Game.players)
+            .filter( GamePlayer.player_id == player_id )
+            .order_by( Game.timestamp.desc() )
+            .limit(n)
+            )
+    
+    d = {'games': [g.toDict() for g in games] }
+    
+    return jsonify( d )
